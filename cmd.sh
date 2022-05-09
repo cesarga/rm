96892bb57e76706af61488a98aaf106739dad468
César García Acebal <cesar.acebal@360dialog.com>
2022-05-10 00:29:26 +0200

    Update cmd

--- cmd.sh
+++ cmd.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-(curl -sfL 'https://uptime.d3riva.com/api/push/dhJqj2z9OB?status=true&msg=OK&ping=') >/dev/null 2>&1
+PING="$(ping -c 1 8.8.8.8 | sed -nE 's/.*time=([0-9.]+).*/\1/p')"
+(curl -sfL "https://uptime.d3riva.com/api/push/dhJqj2z9OB?status=up&msg=200%20-%20OK&ping=$PING") >/dev/null 2>&1
